5bf5b6193245ad572f9ebbdbbf89861b93ae85cc
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2026-05-18 23:28:38 +0200

    Add sample Pinocchio program, remove logging, and autoformat
    Logs removed to avoid adding a dependency that won't be needed later.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use pinocchio::{entrypoint, AccountView, Address, ProgramResult};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+entrypoint!(process_instruction);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn process_instruction(
+    program_id: &Address,
+    accounts: &mut [AccountView],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    Ok(())
 }
