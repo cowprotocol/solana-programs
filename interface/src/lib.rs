928f1b60bc988e6fd52f375bd424ed3d0388bd94
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2026-05-26 13:02:25 +0000

    Use `num_enum` for instruction discriminator parsing (#16)
    Anxo
    [noticed](https://github.com/cowprotocol/solana-programs/commit/4ceeee765d9ebc79db1162bf4642341acf818ecd#r186193998)
    an issue with the
    [changes](https://github.com/cowprotocol/solana-programs/commit/4ceeee765d9ebc79db1162bf4642341acf818ecd)
    addressing [this
    comment](https://github.com/cowprotocol/solana-programs/pull/8#discussion_r3281051579):
    the hand-written `TryFrom<u8> for SettlementInstruction` has two places
    to keep in sync: the explicit `= 0`/`= 1` discriminants on the enum and
    the
    matching `0 => ...`/`1 => ...` arms in the impl. Adding or renumbering a
    variant could silently desynchronise the two.
    
    Here I implement the other half of Tiago's suggestion, that is, use
    `num_enum`.
    
    ## Is `num_enum` safe to add as a dependency?
    
    It looks reasonable:
    
    - [Good adoption stats.](https://lib.rs/crates/num_enum) 12.8M
    downloads/month, ~1.1k direct reverse-dependencies on crates.io; first
    release on 2018; latest release on 2026-03-15; ranked `#20` in the Rust
    patterns category. License: BSD-3-Clause OR MIT OR Apache-2.0.
    - [No RustSec advisories.](https://rustsec.org/search.html?q=num_enum)
    
    ## How to test
    
    I added two unit tests, thought it's a bit overkill.

--- interface/src/lib.rs
+++ interface/src/lib.rs
@@ -1,10 +1,15 @@
 //! Shared types and instruction builders for the CoW Protocol settlement program.
 
 pub use solana_instruction::Instruction;
+use solana_program_error::ProgramError;
 pub use solana_pubkey::Pubkey;
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, num_enum::TryFromPrimitive)]
 #[repr(u8)]
+#[num_enum(error_type(
+    name = ProgramError,
+    constructor = SettlementInstruction::unknown_discriminator,
+))]
 pub enum SettlementInstruction {
     BeginSettle = 0,
     FinalizeSettle = 1,
@@ -14,17 +19,9 @@ impl SettlementInstruction {
     pub fn discriminator(&self) -> u8 {
         *self as u8
     }
-}
 
-impl TryFrom<u8> for SettlementInstruction {
-    type Error = solana_program_error::ProgramError;
-
-    fn try_from(b: u8) -> Result<Self, Self::Error> {
-        match b {
-            0 => Ok(Self::BeginSettle),
-            1 => Ok(Self::FinalizeSettle),
-            _ => Err(Self::Error::InvalidInstructionData),
-        }
+    fn unknown_discriminator(_: u8) -> ProgramError {
+        ProgramError::InvalidInstructionData
     }
 }
 
@@ -41,5 +38,28 @@ pub fn finalize_settle(program_id: &Pubkey) -> Instruction {
         program_id: *program_id,
         accounts: vec![],
         data: vec![SettlementInstruction::FinalizeSettle.discriminator()],
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn settlement_instruction_try_from_partitions_all_bytes() {
+        for i in u8::MIN..=u8::MAX {
+            match SettlementInstruction::try_from(i) {
+                Ok(ix) => assert_eq!(ix as u8, i),
+                Err(err) => assert_eq!(err, ProgramError::InvalidInstructionData),
+            }
+        }
+    }
+
+    #[test]
+    fn settlement_instruction_try_from_matches_begin_settle() {
+        assert_eq!(
+            SettlementInstruction::try_from(0),
+            Ok(SettlementInstruction::BeginSettle)
+        );
     }
 }
